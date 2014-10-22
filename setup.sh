92a553debfe706728965541127e6c9de8f7154f4
Dan Barber <github@danbarber.me>
2014-10-22 22:01:13 +0100

    Tap boneyard.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # We want to be in the user home directory.
 cd ~
 
+# Tap boneyard for bundle command
+brew tap homebrew/boneyard
+
 # Get Brewfiles.
 echo 'Downloading Brewfiles...'
 curl -s https://raw.githubusercontent.com/danbee/mac-setup/master/Brewfile > ~/Brewfile
