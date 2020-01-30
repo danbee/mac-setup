0a0df8efb65d5e3d9f724e2368bef7eb4b4bf5de
Daniel Barber <github@danbarber.me>
2020-01-30 11:17:29 -0500

    Annotate MAS installs

--- lib/mas.sh
+++ lib/mas.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # mas install 13764025890
-mas install 1107421413
-mas install 403504866
-mas install 1278508951
-mas install 497799835
-mas install 1179623856
-mas install 1082624744
+mas install 1107421413 # 1Blocker
+mas install 403504866  # PCalc
+mas install 1278508951 # Trello
+mas install 497799835  # Xcode
+mas install 1179623856 # Pastebot
+mas install 1082624744 # Gifox
+mas install 1451685025 # Wireguard
